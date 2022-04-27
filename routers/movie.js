24e0b94635b3dc5972378391e4e2b17d43bec899
leo37d2 <keyz2609@gmail.com>
2022-04-27 17:00:51 +0700

    improve code for filter

--- routers/movie.js
+++ routers/movie.js
@@ -49,8 +49,8 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 // search by name
 
 router.get("/search", async (req, res) => {
-  console.log(req.query.q);
-  const qSearch = new RegExp(req.query.q, "i");
+  const qSearch = new RegExp(req.query.q.replace("+", " "), "i");
+  console.log(qSearch);
   if (qSearch) {
     try {
       let movie = await Movie.find({
@@ -83,119 +83,73 @@ router.get("/find/:id", async (req, res) => {
 
 // get all movie
 
-router.get("/", async (req, res) => {
+router.get("/all", async (req, res) => {
+  let perPage = 16;
+  let page = req.query.page || 1;
   try {
-    const movie = await Movie.find();
-    res.status(200).json(movie);
+    await Movie.aggregate([
+      {
+        $project: {
+          language: 0,
+          ticket_price: 0,
+          date: 0,
+          description: 0,
+          singer_actor_id: 0,
+        },
+      },
+    ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec((error, movie) => {
+        Movie.countDocuments((error, count) => {
+          if (error) return res.status(401).json(error);
+
+          res.header({
+            "Page-size": Math.ceil(count / perPage),
+            "Current": page,
+          });
+          res.status(200).json(movie);
+        });
+      });
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-// filter one by one
-
-//filter by genre
-const fGenre = (query) =>
-  Movie.find({
-    genre: {
-      $in: [query],
-    },
-  });
-// filter by date
-const fDate = (query) =>
-  Movie.find({
-    date: {
-      $in: [query],
-    },
-  });
-// filter by city
-const fCity = (query) =>
-  Movie.find({
-    city: {
-      $in: [qCity],
-    },
-  });
-// filter by state
-const fState = (query) =>
-  Movie.find({
-    state: query,
-  });
-// filter by genre & date
-const fGD = (query1, query2) =>
-  Movie.find({
-    genre: {
-      $in: [query1],
-    },
-    date: {
-      $in: [query2],
-    },
-  });
-// filter by genre & city
-const fGC = (query1, query2) =>
-  Movie.find({
-    genre: {
-      $in: [query1],
-    },
-    city: {
-      $in: [query2],
-    },
-  });
-// filter by genre & state
-const fGS = (query1, query2) =>
-  Movie.find({
-    genre: {
-      $in: [query1],
-    },
-    state: query2,
-  });
-// filter by date & city
-const fDC = (query1, query2) =>
-  Movie.find({
-    date: {
-      $in: [query1],
-    },
-    city: {
-      $in: [query2],
-    },
-  });
-// filter by  date & state
-const fDS = (query1, query2) =>
-  Movie.find({
-    date: {
-      $in: [query1],
-    },
-    state: query2,
-  });
-// filter by city & state
-const fCS = (query1, query2) =>
-  Movie.find({
-    city: {
-      $in: [query1],
-    },
-    state: query2,
-  });
+// filter
 router.get("/filter", async (req, res) => {
-  const qGenre = req.query.genre;
-  const qDate = new Date(req.query.date);
-  const qCity = req.query.city;
-  const qState = req.query.state;
+  const filter = req.query;
+  const movie = await Movie.find();
+
   try {
-    let list;
-    if (qGenre && qDate) list = await fGD(qGenre, qDate);
-    else if (qGenre && qCity) list = await fGC(qGenre, qCity);
-    else if (qGenre && qState) list = await fGS(qGenre, qState);
-    else if (qDate && qCity) list = await fDC(qDate, qCity);
-    else if (qDate && qState) list = await fDS(qDate, qState);
-    else if (qCity && qState) list = await fCS(qCity, qState);
-    else if (qGenre) list = await fGenre(qGenre);
-    else if (qDate) list = await fDate(qDate);
-    else if (qCity) list = await fCity(qCity);
-    else if (qState) list = await fState(qState);
-    else list = await Movie.find();
+    const list = movie.filter(function (item) {
+      for (var key in filter) {
+        if (item[key] === undefined || item[key] != filter[key]) return false;
+      }
+      return true;
+    });
     res.status(200).json(list);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
+// filter v2
+router.get("/filterv2", async (req, res)=>{
+  const queryCond={}
+  if(req.query.city)
+    queryCond.city=req.query.city.replace("+"," ")
+  if(req.query.state)
+    queryCond.state=req.query.state.replace("+", " ")
+    if(req.query.year)
+      queryCond.year=req.query.year
+    if(req.query.genre)
+      queryCond.genre=req.query.genre
+  try {
+    const movie = await Movie.find(queryCond)
+    res.status(200).json(movie)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 module.exports = router;
